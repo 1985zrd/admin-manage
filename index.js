66debcfbbe47159a6668ed766afc088ef45f0daa
rende.zhang <rende.zhang@ikang.com>
2020-08-20 15:14:57 +0800

    base

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-http.createServer((req, res) => {
-  res.writeHead(200, {
-    'content-type': 'text/plain'
-  })
-  res.write('hello server')
-  res.end()
-}).listen(80)
